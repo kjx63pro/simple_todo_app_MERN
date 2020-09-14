28a4c7ae9c9d7791ddc091d48c74a7994978d880
kjx63pro <kjx63pro@gmail.com>
2020-09-14 20:14:50 +0900

    Create Todo Model and get all todos

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const Todo = require('../../models/Todo');
+
 // @route     GET api/todos
 // @desc      Get all todos
 // @access    Public
-
-router.get('/', (req, res) => {
-  res.send('Get all todos');
+router.get('/', async (req, res) => {
+  try {
+    const todos = await Todo.find({});
+    res.json(todos);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 module.exports = router;
